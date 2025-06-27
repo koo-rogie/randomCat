becc4623ccecc9482e72a907da2d560a3f463a50
koo-rogie <rn5184@naver.com>
2025-06-28 03:41:07 +0900

    style(footer): apply CSS module styling and remove Sam component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,12 @@
-import Sam from "@/components/Sam";
+import styles from "../styels/footer.module.css";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <div className="inner">
         <p>안녕하세요</p>
         <p>고양이 렌덤 사진 불러오기 사이트에 오신것을 환영합니다!</p>
       </div>
-      <Sam />
     </footer>
   );
 }
