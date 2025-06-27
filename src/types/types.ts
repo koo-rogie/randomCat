0564c218412c340a05a36d5f53037f6c762ef4e3
koo-rogie <rn5184@naver.com>
2025-06-28 02:49:53 +0900

    chore: add type definitions

---
+++ src/types/types.ts
@@ -0,0 +1,22 @@
+export interface Cat {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface CatList {
+  items: Cat[][];
+}
+
+export type State = {
+  items: CatList["items"];
+  loading: boolean;
+  error: string | null;
+};
+
+export type Action = { type: "FETCH_INIT" }
+  | { type: "FETCH_SUCCESS"; payload: CatList["items"] }
+  | { type: "FETCH_FAILURE"; payload: string };
+
+
