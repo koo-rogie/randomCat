8ed7ab1e578d105adca264836c6009f9d047d91b
koo-rogie <rn5184@naver.com>
2025-06-28 02:50:21 +0900

    feat: add page components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="inner">
+      <p>안녕하세요 고양이 랜덤 생성기 api 태스트입니다.</p>
+    </div>
+  );
+}
---
+++ src/pages/RandomCat.tsx
@@ -0,0 +1,56 @@
+import style from "../styels/RandomCat.module.css";
+import useAxiosInstance from "@/hooks/useAxios";
+import React, { useEffect, useReducer } from "react";
+import reducer from "@/hooks/reducer";
+
+function RandomCat() {
+  console.log("고양이 리스트 불러오기");
+  const axiosInstance = useAxiosInstance();
+  const [state, dispatch] = useReducer(reducer, { items: [], loading: false, error: null });
+
+  const axiosCatList = async () => {
+    dispatch({ type: "FETCH_INIT" });
+    try {
+      const res = await axiosInstance.get("/search");
+      dispatch({ type: "FETCH_SUCCESS", payload: res.data });
+      console.log(res.data);
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : "Unknown error";
+      dispatch({ type: "FETCH_FAILURE", payload: errorMessage });
+      alert("고양이 불러오기에 실패했습니다.");
+    }
+  };
+
+  const itemList = state.items.flat().map((item) => (
+    <li key={item.id}>
+      <img src={item.url} alt={item.id} />
+    </li>
+  ));
+
+  const addItem = () => {
+    console.log("클릭");
+    axiosCatList();
+  };
+
+  useEffect(() => {
+    addItem();
+  }, []);
+
+  return (
+    <section>
+      <div className="inner">
+        <div className={`${style["RandomCat-header"]}`}>
+          <p className={`${style["RandomCat-header-title"]}`}>렌덤 고양이 화면입니다</p>
+          <button className={style.button} onClick={addItem}>
+            버튼을 클릭하여 고양이 사진 불러오기
+          </button>
+        </div>
+        <div className={`${style["RandomCat-content"]}`}>
+          <ul>{itemList}</ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default React.memo(RandomCat);
