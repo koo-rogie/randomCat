00f74f84469708a5d776d97490d896c120f1924e
koo-rogie <rn5184@naver.com>
2025-06-29 01:07:54 +0900

    feat(router): add ErrorPage errorElement and update child routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,17 @@
 import Layout from "@/components/Layout";
 import Home from "@/pages/Home";
 import RandomCat from "@/pages/RandomCat";
+import ErrorPage from "@pages/ErrorPage";
 import { createBrowserRouter, Navigate } from "react-router";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "home", element: <Navigate to="/" /> },
-      { path: "home", element: <Home /> },
+      { path: "/", element: <Home /> },
       { path: "randomcat", element: <RandomCat /> },
     ],
   },
